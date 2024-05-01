49fa0058b4ff333f073304a45bc89586babd2ea0
cttia <ctia934@gmail.com>
2024-05-01 15:23:16 -0700

    theme specification

--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -1,24 +1,19 @@
 import 'package:flutter/material.dart';
 
-ThemeData lightmode=ThemeData(
-  brightness: Brightness.light,
-  colorScheme: ColorScheme.light(
-    background: Color.fromARGB(255, 151, 156, 162),
-    primary: Color.fromARGB(255, 153, 168, 227),
-    secondary: Color.fromARGB(252, 58, 55, 101),
-    tertiary: Color.fromARGB(255, 232, 238, 244)
-  )
-);
+// light mode theme with specific colors for background, primary, secondary, and tertiary elements.
+ThemeData lightmode = ThemeData(
+    brightness: Brightness.light,
+    colorScheme: ColorScheme.light(
+        background: Color.fromARGB(255, 151, 156, 162),
+        primary: Color.fromARGB(255, 153, 168, 227),
+        secondary: Color.fromARGB(252, 58, 55, 101),
+        tertiary: Color.fromARGB(255, 232, 238, 244)));
 
-
-
-
-ThemeData darkMode=ThemeData(
-  brightness: Brightness.dark,
-  colorScheme: ColorScheme.dark(
-    background: Color.fromARGB(255, 3, 1, 34),
-    primary: Color.fromARGB(248, 71, 71, 217),
-    secondary: Color.fromARGB(255, 50, 0, 89),
-    tertiary: Color.fromARGB(255, 7, 29, 58)
-  )
-);
+//dark mode theme with specific colors for background, primary, secondary, and tertiary elements.
+ThemeData darkMode = ThemeData(
+    brightness: Brightness.dark,
+    colorScheme: ColorScheme.dark(
+        background: Color.fromARGB(255, 3, 1, 34),
+        primary: Color.fromARGB(248, 71, 71, 217),
+        secondary: Color.fromARGB(255, 50, 0, 89),
+        tertiary: Color.fromARGB(255, 7, 29, 58)));
