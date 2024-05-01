691807591a55eddbd4a3a6318f33052ed24c51e4
Betty-SWE <betelhemadmasu77@gmail.com>
2024-05-02 00:13:35 +0400

    button edit

--- lib/components/button.dart
+++ lib/components/button.dart
@@ -1,26 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:musica/const/listTextStyle.dart';
 
-class Inputbutton extends StatelessWidget {
+class InputButton extends StatelessWidget {
+  final Function()? onTap; // Callback function to execute when the button is tapped
+  final String btnName; // The text displayed on the button
 
+  InputButton({super.key, required this.onTap, required this.btnName});
 
-  final Function()? onTap;
-  final String btnName;
- Inputbutton({super.key, required this.onTap, required this.btnName});
   @override
   Widget build(BuildContext context) {
-    return  GestureDetector(
-      onTap: onTap,
+    return GestureDetector(
+      onTap: onTap, // Assign the provided onTap function to the GestureDetector
       child: Container(
         height: 60,
         width: 100,
         decoration: BoxDecoration(
-          color: Colors.blue
+          color: Colors.blue, // Set the background color of the container
         ),
         child: Center(
           child: Text(
-            btnName,
-            style: Ourstyle(),
+            btnName, // Display the provided button name
+            style: Ourstyle(), // Apply the specified text style (Ourstyle)
           ),
         ),
       ),
