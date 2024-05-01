cacc4c6c196120a21e34488ad680d892e2493b21
Betty-SWE <betelhemadmasu77@gmail.com>
2024-05-02 00:55:53 +0400

    textstyle

--- lib/components/textsty.dart
+++ lib/components/textsty.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:musica/const/listTextStyle.dart';
 
+/// Custom widget for displaying text with optional ellipsis and bold styling.
+///
+
 Text Namess( { int mxln=1,  String text="",bool IsBold=false}){
   return Text(
     text,
