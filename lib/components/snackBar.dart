a5f2bedf3f5ba4b08dfe3253376cd05b3723bec4
Betty-SWE <betelhemadmasu77@gmail.com>
2024-05-02 00:57:54 +0400

    snack bar paramets update

--- lib/components/snackBar.dart
+++ lib/components/snackBar.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 
 /// Creates a custom SnackBar widget with the specified [textContent] and [duration].
 ///
+////// Parameters:
+/// - [textContent]: The text content to display in the SnackBar.
+/// - [duration]: The duration for which the SnackBar should be visible (in seconds).
+/// 
 SnackBar snakB({required String texContent,required int duration}){ return SnackBar(
                                   content: Text(texContent),
                                   duration: Duration(seconds: duration),
