6c89cfdb5e196b026151572214f781b34a1348d0
Betty-SWE <betelhemadmasu77@gmail.com>
2024-05-02 00:58:16 +0400

    return properties

--- lib/components/snackBar.dart
+++ lib/components/snackBar.dart
@@ -5,7 +5,8 @@ import 'package:flutter/material.dart';
 ////// Parameters:
 /// - [textContent]: The text content to display in the SnackBar.
 /// - [duration]: The duration for which the SnackBar should be visible (in seconds).
-/// 
+/// Returns:
+/// A SnackBar widget with the specified properties.
 SnackBar snakB({required String texContent,required int duration}){ return SnackBar(
                                   content: Text(texContent),
                                   duration: Duration(seconds: duration),
