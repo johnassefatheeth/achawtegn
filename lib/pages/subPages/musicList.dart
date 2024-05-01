ab51f888f28c0e61b1366b2115171285125f02eb
John Assefa <johnassefaforbussines@gmail.com>
2024-05-01 06:09:15 -0700

    making the options popup

--- lib/pages/subPages/musicList.dart
+++ lib/pages/subPages/musicList.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:musica/components/custappBar.dart';
+import 'package:musica/components/musdicOptions.dart';
 import 'package:musica/components/textsty.dart';
 import 'package:musica/const/icon.dart';
 import 'package:musica/const/listTextStyle.dart';
@@ -61,99 +62,7 @@ class _PlaylistSongsState extends State<PlaylistSongs> {
                                           Icons.play_arrow,
                                           size: 26,
                                         )
-                                            : GestureDetector(
-                                              onTap: () {
-                                                  showModalBottomSheet<void>(
-                                                    context: context,
-                                                    builder: (BuildContext context) {
-                                                      return Container(
-                                                        padding: EdgeInsets.all(10.0),
-                                                        child: Column(
-                                                          mainAxisSize: MainAxisSize.min,
-                                                          children: <Widget>[
-                                                            ListTile(
-                                                              title: Text('Rename'),
-                                                              onTap: () {
-                                                                Navigator.pop(context);
-                                                              },
-                                                            ),
-                                                            ListTile(
-                                                              title:const Text('Delete',
-                                                              style: TextStyle(
-                                                                color: Colors.red,
-                                                              ),
-                                                              ),
-                                                              onTap: () async {
-                                                                await showDialog(
-                                                                  context: context,
-                                                                  builder: (BuildContext context) {
-                                                                    return AlertDialog(
-                                                                      title: Text('Delete Song'),
-                                                                      content: Text('Are you sure you want to delete this song?'),
-                                                                      actions: [
-                                                                        TextButton(
-                                                                          onPressed: () =>{Navigator.pop(context, false)} ,
-                                                                          child: Text('Cancel'),
-                                                                        ),
-                                                                    TextButton(
-                                                                      onPressed: () => Navigator.pop(context, true),
-                                                                      child: Text('Delete'),
-                                                                    ),
-                                                                  ],
-                                                                );})                 
-                                    ;
-                                                                final file = File( Uri.parse(song.uri!).toString());
-                                                                  if (await file.exists()) {
-                                                                    file.delete();
-                                                                  } else {
-                                                                    print('Error: File not found');
-                                                                  }
-                                                                Navigator.pop(context);
-                                                              },
-                                                            ),
-                                                            ListTile(
-                                                              title: Text('Set as ringtone'),
-                                                              onTap: () {
-                                                                // Handle set as ringtone option
-                                                                
-                                                                
-                                                                print('Set as ringtone option tapped');
-                                                              },
-                                                            ),
-                                                            ListTile(
-                                                              title: Text('Properties'),
-                                                              onTap: () async {
-                                                                await showDialog(
-                                                                  context: context,
-                                                                  builder: (BuildContext context) {
-                                                                    return AlertDialog(
-                                                                      title: Text('Song Info'),
-                                                                      content: Column(
-                                                                        children: [
-                                                                          Text('title-'+song.displayName),
-                                                                          Text('type-'+song.fileExtension),
-                                                                          Text('path-'+song.uri.toString()),
-                                                                          Text('date added-'+song.dateAdded.toString()),
-                                                                          Text('last modified-'+song.dateModified.toString()),
-                                                                        ],
-                                                                      ),
-                                                                      
-                                                                );
-                                                                }
-                                                                );
-                                                              },
-                                                            ),
-                                                          ],
-                                                        ),
-                                                      );
-                                                    },
-                                                  );
-                                                },
-                                              child: const Icon(
-                                                  Icons.more_vert,
-                                                  size: 26,
-                                                ),
-                                            ),
+                                            : options(song:song,context:context),
                       onTap: () {
                            controller.playSong(song, index);
                           Navigator.of(context).pushReplacement(
