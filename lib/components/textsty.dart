6b9359a906260a7559ba7fd00793f854855240d4
Betty-SWE <betelhemadmasu77@gmail.com>
2024-05-02 00:56:22 +0400

    text style parameters

--- lib/components/textsty.dart
+++ lib/components/textsty.dart
@@ -3,6 +3,11 @@ import 'package:musica/const/listTextStyle.dart';
 
 /// Custom widget for displaying text with optional ellipsis and bold styling.
 ///
+/// Parameters:
+/// - [mxln]: The maximum number of lines for the text (default is 1).
+/// - [text]: The text content to display.
+/// - [IsBold]: Whether the text should be displayed in bold (default is false).
+///
 
 Text Namess( { int mxln=1,  String text="",bool IsBold=false}){
   return Text(
