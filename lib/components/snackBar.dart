6d20edb59a904c4be4f19ece516a8c055289e317
Betty-SWE <betelhemadmasu77@gmail.com>
2024-05-02 00:57:26 +0400

    snackbar

--- lib/components/snackBar.dart
+++ lib/components/snackBar.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+/// Creates a custom SnackBar widget with the specified [textContent] and [duration].
+///
 SnackBar snakB({required String texContent,required int duration}){ return SnackBar(
                                   content: Text(texContent),
                                   duration: Duration(seconds: duration),
